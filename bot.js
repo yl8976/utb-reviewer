908b5cabce4451a80bf0e60a20fea6300c9904ee
Young Lee <jaeyoung-lee@hotmail.com>
2018-10-09 23:03:52 +0100

    update

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@
 // })
 
 
-var Twit = require(‘twit’);
-var TwitterBot = require(‘node-twitterbot’).TwitterBot;
+var Twit = require('twit');
+var TwitterBot = require('node-twitterbot').TwitterBot;
 var Bot = new TwitterBot({
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
