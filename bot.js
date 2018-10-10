8e3dcfc6dabc118ed63a315e78a4a81ca3cf59de
Young Lee <jaeyoung-lee@hotmail.com>
2018-10-10 01:08:09 +0100

    update

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.PROJECT_ID,
         clientEmail: process.env.CLIENT_EMAIL,
-        privateKey: process.env.PRIVATE_KEY
+        privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
     }),
     databaseURL: "https://utb-reviewer.firebaseio.com"
 });
