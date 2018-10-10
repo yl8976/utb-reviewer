d3f7d2dd11c68569df28439541269829cd8052ab
Young Lee <jaeyoung-lee@hotmail.com>
2018-10-10 01:41:31 +0100

    update

--- bot.js
+++ bot.js
@@ -1,28 +1,17 @@
+// Import Firebase Admin, Twit, and Node-Twitterbot
 var admin = require('firebase-admin');
 var Twit = require('twit');
 var TwitterBot = require('node-twitterbot').TwitterBot;
 
+// Setup Twitter API access (secret keys are stored privately on Heroku)
 var Bot = new TwitterBot({
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
     access_token: process.env.BOT_ACCESS_TOKEN,
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = ["This post is SO good, 10/10 ",
-    "Hahahahahahahahahahaha 10/10 ",
-    "lmao 10/10 ",
-    "absolute 10/10 ",
-    "lmfao 10/10 ",
-    "ok, this is epic 10/10 ",
-    "LOL same 10/10 ",
-    "yes 10/10 ",
-    "xD 10/10 ",
-    "wow. 10/10 "
-];
-function chooseRandom(myArray) {
-    return myArray[Math.floor(Math.random() * myArray.length)];
-}
-var phrase = chooseRandom(phraseArray);
+
+// Initialize Firebase database
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.PROJECT_ID,
@@ -31,15 +20,38 @@ admin.initializeApp({
     }),
     databaseURL: "https://utb-reviewer.firebaseio.com"
 });
-
 var db = admin.database();
 var ref = db.ref("links/most-recent-link");
-var mostRecentPost = "";
+var mostRecentPostLink = "";
 
+// Random phrases to choose from
+var phraseArray = ["This post is SO good, 10/10 ",
+    "Hahahahahahahahahahaha 10/10 ",
+    "lmao 10/10 ",
+    "absolute 10/10 ",
+    "lmfao 10/10 ",
+    "ok, this is epic 10/10 ",
+    "LOL same 10/10 ",
+    "yes 10/10 ",
+    "jeeeeeeeez you really outdid yourself with this one. 10/10 ",
+    "wow. 10/10 "
+];
+
+// Chooses a random phrase to tweet
+function chooseRandom(myArray) {
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
+var phrase = chooseRandom(phraseArray);
+
+// Setup Puppeteer, a headless Chrome browser
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const url = 'https://www.underthebutton.com/section/all';
+
+// Most recent post links
 var links = []
+
+// Launch puppeteer and scrape https://underthebutton.com's Most Recent page
 puppeteer
     .launch({
         args: ['--no-sandbox']
@@ -53,23 +65,26 @@ puppeteer
         });
     })
     .then(function (html) {
+        // Grabs only links from Most Recent page
         $('.most-recent-photo > a', html).each(function () {
-            console.log($(this).attr("href"));
             links.push($(this).attr("href"));
         });
         ref.set({
             link: links[0],
         });
+        console.log("The most recent link so far: " + links[0])
     })
     .catch(function (err) {
-        console.log("Error!");
+        // Log error to Heroku console
+        console.log("You've got an error. Check it out below:");
         console.log(err);
     });
 
-// Get the data on a post that has changed
+// If most recent link changes when you scrape the site, compose a new tweet with the latest post link
 ref.on("child_changed", function (snapshot) {
     var changedPost = snapshot.val();
-    console.log("The most recent UTB post is " + changedPost);
-    mostRecentPost = String(changedPost);
-    Bot.tweet(phrase + mostRecentPost);
+    console.log("New post detected! The most recent post link is: " + changedPost);
+    mostRecentPostLink = String(changedPost);
+    Bot.tweet(phrase + mostRecentPostLink);
+    console.log("Tweet successful. The tweet says: " + phrase);
 });
